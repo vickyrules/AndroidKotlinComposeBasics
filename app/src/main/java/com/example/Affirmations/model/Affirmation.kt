2f8575df3cba09d144d0ee02fc39b4d173522864
vickyrules <VICKYRULES1705@GMAIL.COM>
2023-02-16 20:43:39 +0530

    woof project start

--- app/src/main/java/com/example/Affirmations/model/Affirmation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.Affirmations.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val drawableResourceId: Int
-)
