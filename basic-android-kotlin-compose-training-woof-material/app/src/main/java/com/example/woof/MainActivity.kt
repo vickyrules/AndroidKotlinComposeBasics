87d266eb65039b948f6ca228553c784e50535ec4
vickyrules <VICKYRULES1705@GMAIL.COM>
2023-02-19 16:19:39 +0530

    Woof App Basic Animation

--- basic-android-kotlin-compose-training-woof-material/app/src/main/java/com/example/woof/MainActivity.kt
+++ basic-android-kotlin-compose-training-woof-material/app/src/main/java/com/example/woof/MainActivity.kt
@@ -1,39 +1,26 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.woof
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,6 +33,12 @@ import androidx.compose.ui.unit.dp
 import com.example.woof.data.Dog
 import com.example.woof.data.dogs
 import com.example.woof.ui.theme.WoofTheme
+import androidx.compose.material.Icon
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,6 +54,7 @@ class MainActivity : ComponentActivity() {
 /**
  * Composable that displays an app bar and a list of dogs.
  */
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun WoofApp() {
     Scaffold(
@@ -76,18 +70,74 @@ fun WoofApp() {
     }
 }
 
+
+@Composable
+fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            painter = if (expanded) painterResource(R.drawable.baseline_expand_less_24) else painterResource(
+                R.drawable.baseline_expand_more_24
+            ),
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(R.string.expand_button_content_description)
+        )
+    }
+}
+
 /**
  * Composable that displays a list item containing a dog icon and their information.
  *
  * @param dog contains the data that populates the list item
  * @param modifier modifiers to set to this composable
  */
+
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    var Green25 = Color(0xFF86efac)
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) Green25 else MaterialTheme.colors.surface,
+    )
     Card(
         elevation = 4.dp,
         modifier = modifier.padding(8.dp)
     ) {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+                .background(color=color)
+        )
+        {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -95,9 +145,18 @@ fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
             ) {
                 DogIcon(dog.imageResourceId)
                 DogInformation(dog.name, dog.age)
+                Spacer(Modifier.weight(2f))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded })
+            }
+            if (expanded) {
+                DogHobby(dog.hobbies)
+            }
         }
     }
 }
+
 
 /**
  * Composable that displays a Top Bar with an icon and text.
@@ -182,7 +241,7 @@ fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Mo
 @Preview
 @Composable
 fun WoofPreview() {
-    WoofTheme(darkTheme = false) {
+    WoofTheme() {
         WoofApp()
     }
 }
@@ -190,10 +249,3 @@ fun WoofPreview() {
 /**
  * Composable that displays what the UI of the app looks like in dark theme in the design tab.
  */
-@Preview
-@Composable
-fun WoofDarkThemePreview() {
-    WoofTheme(darkTheme = true) {
-        WoofApp()
-    }
-}
